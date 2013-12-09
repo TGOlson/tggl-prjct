d080353c4b55e938832005515683e5c6fd0a5309
TGOlson <tydotg@gmail.com>
2013-12-08 21:43:55 -0800

    working on layous js

---
+++ layout.js
@@ -0,0 +1,25 @@
+var Layout = {}
+
+Layout.setPreLabels = function(){
+	// var buttons = Layout.findAllButtons()
+	// for(var i = 0; i < buttons.length; i++){
+	// 	var buttonClass = Layout.getButtonClass(buttons[i])
+	// 	Layout.appendClass(buttons[i], buttonClass)
+	// }
+}
+
+Layout.findAllButtons = function(){
+	// return document.querySelectorAll('.toggle-button')
+}
+
+Layout.getButtonClass = function(button){
+	// return button.className
+}
+
+Layout.appendClass = function(elem, klass){
+	// elem.parentNode.innerHTML += '<pre>' + klass + '</pre>'
+}
+
+// window.onload = function() {
+// 	// Layout.setPreLabels()
+// }
