005c3eec39f03ffb37887f820ab9dff86dd9a0fc
TGOlson <tydotg@gmail.com>
2013-12-13 23:32:44 -0800

    show class names when toggled

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,19 @@
 var Layout = {}
 
 Layout.init = function(){
+	Layout.setToggleEvents()
+	Layout.setDisplayTags()
+}
+
+Layout.setToggleEvents = function(){
+	document.addEventListener('click', Layout.setDisplayTags)
+},
+
+Layout.setDisplayTags = function(){
 	var buttons = Layout.getAllClassTags('.toggle-button')
 	var circles = Layout.getAllClassTags('.toggle-circle')
 	Layout.appendClassTagsToDivs(buttons, circles)
-}
+},
 
 Layout.getAllClassTags = function(klass){
 	var elems = Layout.findAll(klass)
@@ -26,7 +35,7 @@ Layout.mapListToClasses = function(elems){
 Layout.appendClassTagsToDivs = function(buttons, circles){
 	var cols = Layout.findAll('.info')
 	for(var i = 0; i < cols.length; i++){
-		cols[i].innerHTML += '<pre>' + buttons[i] + '</pre>' + 
+		cols[i].innerHTML = '<pre>' + buttons[i] + '</pre>' + 
 	 						 '<pre>' + circles[i] + '</pre>'
 	}
 }
