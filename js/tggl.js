27610d7f9fbef121c13d943546d9c05615697fcb
TGOlson <tydotg@gmail.com>
2014-01-18 21:24:12 -0800

    add tggl modify for namespacing

--- js/tggl.js
+++ js/tggl.js
@@ -1,7 +1,7 @@
 var SlideToggle = {}
 
 SlideToggle.setEventListeners = function(){
-		var buttons = document.querySelectorAll('.toggle-button')
+		var buttons = document.querySelectorAll('.tggl-button')
 		for(var i = 0; i < buttons.length; i++){
 			buttons[i].addEventListener('click', SlideToggle.toggleClick)
 		}
@@ -14,7 +14,7 @@ SlideToggle.toggleClick = function(e){
 
 SlideToggle.closestToggleButton = function(elem){
 	while(elem != undefined){
-		if(SlideToggle.hasClass(elem, 'toggle-button')){
+		if(SlideToggle.hasClass(elem, 'tggl-button')){
 			return elem
 		}
 		elem = elem.parentNode
@@ -26,20 +26,20 @@ SlideToggle.hasClass = function(elem, klass){
 }
 
 SlideToggle.toggle = function(button){
-	if(SlideToggle.hasClass(button, 'toggled')){
+	if(SlideToggle.hasClass(button, 'tggl-toggled')){
 		SlideToggle.toggleLeft(button)
-	} else { 
+	} else {
 		SlideToggle.toggleRight(button)
 	}
 }
 
 SlideToggle.toggleRight = function(button){
-	button.className = button.className + ' toggled'
+	button.className = button.className + ' tggl-toggled'
 }
 
 SlideToggle.toggleLeft = function(button){
 	var buttonClass = button.className.split(' ')
-	var oldClassPos = buttonClass.indexOf('toggled')
+	var oldClassPos = buttonClass.indexOf('tggl-toggled')
 	buttonClass.splice(oldClassPos, 1)
 	button.className = buttonClass.join(' ')
 }
